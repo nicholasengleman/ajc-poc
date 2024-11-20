e62bc91519103ced065bd4b58b45926727e8deff
Nicholas Engleman <nicholas.engleman@ajc.com>
2024-11-20 15:49:02 -0500

    moved layout file

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
       "cloudfront-us-east-1.images.arcpublishing.com",
       "s3.amazonaws.com",
       "author-service-images-prod-us-east-1.publishing.aws.arc.pub",
+      "https://d1fegwn2wjh0cs.cloudfront.net",
     ],
   },
 };
